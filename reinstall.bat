214223cc3cd61b37125b2ca747dfaba9c35dcb8b
Jason Yang <18200748882@163.com>
2026-03-22 13:09:13 +0800

    jueti fork

--- reinstall.bat
+++ reinstall.bat
@@ -2,8 +2,8 @@
 mode con cp select=437 >nul
 setlocal EnableDelayedExpansion
 
-set confhome=https://raw.githubusercontent.com/bin456789/reinstall/main
-set confhome_cn=https://cnb.cool/bin456789/reinstall/-/git/raw/main
+set confhome=https://raw.githubusercontent.com/jueti/reinstall/main
+set confhome_cn=https://gitea.myclocd.host:16666/jueti/reinstall/raw/branch/main/
 rem set confhome_cn=https://www.ghproxy.cc/https://raw.githubusercontent.com/bin456789/reinstall/main
 
 set pkgs=curl,cpio,p7zip,dos2unix,jq,xz,gzip,zstd,openssl,bind-utils,libiconv,binutils
